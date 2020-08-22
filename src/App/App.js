158ea5d2e54d2a60b421755f9c5ae8a40b6f5606
Ryan Beiden <rdbeiden@gmail.com>
2020-08-22 13:49:10 -0500

    initial setup and structure creation

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <h1>Cat Cafe</h1>
+        </center>
+      </div>
+    );
+  }
+}
+
+export default App;
